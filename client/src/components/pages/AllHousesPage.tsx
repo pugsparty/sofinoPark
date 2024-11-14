563e7d3807ad61af68de08d0fa579e1c5e352440
Ilya <papondreo@mail.ru>
2024-11-14 17:30:55 +0300

    123

--- client/src/components/pages/AllHousesPage.tsx
+++ client/src/components/pages/AllHousesPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography, Card, CardMedia, CardContent } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 type CottageType = {
   id: number;
