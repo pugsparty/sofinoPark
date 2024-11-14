2c259c268a967941c7c5935451511e700e3d35b8
Ilya <papondreo@mail.ru>
2024-11-14 17:12:14 +0300

    release houses

--- client/src/components/UI/HousePage.tsx
+++ client/src/components/UI/HousePage.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { Typography, Card, CardMedia, CardContent } from '@mui/material';
+
+type CottageType = {
+  id: number;
+  photo: string;
+  price: string;
+  area: string;
+  description: string;
+};
 
 export default function HousePage(): React.JSX.Element {
-  return <div>HousePage</div>;
+  const location = useLocation();
+  const cottage = location.state?.cottage as CottageType;
+
+  return (
+    <Card sx={{ maxWidth: 1900, margin: 'auto', padding: 2, marginTop: 4 }}>
+      <CardMedia component="img" height="800" image={cottage.photo} alt="Cottage photo" />
+      <CardContent>
+        <Typography variant="h5">{cottage.price}</Typography>
+        <Typography variant="subtitle1" color="text.secondary">{cottage.area}</Typography>
+        <Typography variant="body1" color="text.secondary" style={{ marginTop: 10 }}>
+          {cottage.description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
