2c9f4ebf27922023be832960d42416d87a753208
pugsparty <luka61097@yandex.ru>
2024-11-14 16:05:03 +0300

    release footer

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import NavBar from './UI/NavBar';
 import { Outlet } from 'react-router-dom';
+import Footer from './UI/Footer';
 
 export default function Layout(): React.JSX.Element {
   return (
-    // <div>Layout</div>
-    <div>
+    <>
       <NavBar />
       <Outlet />
-    </div>
+      <Footer />
+    </>
   );
 }
--- client/src/components/pages/ContactsPage.tsx
+++ client/src/components/pages/ContactsPage.tsx
@@ -1,36 +1,34 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import '../../Styles/ContactsPageStyle.css';
 
 export default function ContactsPage(): React.JSX.Element {
   return (
     <div className="container">
-      <h1 className="presentation">ПОЛУЧИТЕ ПРЕЗЕНТАЦИЮ</h1>
-      <h2 className="contacts">КОНТАКТЫ:</h2>
-      <h2 className="phone">+7 (915) 141-97-77</h2>
-      <h2 className="email">SOFINOPARK@YANDEX.RU</h2>
+      <div className="text">
+        <h1 className="contacts">КОНТАКТЫ:</h1>
+        <h2 className="phone">+7 (915) 141-97-77</h2>
+        <h2 className="email">SOFINOPARK@YANDEX.RU</h2>
+      </div>
 
-      <span className="disclaimer">
-        Получить презентациюПредоставленная информация (в том числе визуализации) на сайте не
-        является публичной офертой. Приведенные данные, информация могут быть изменены. Визуализации
-        могут отличаться от публикуемых изображений. Наличие участков, актуальные цены и данные
-        уточняйте в отделе продаж.
-      </span>
-      <TextField
-        id="standard-textarea"
-        label="Введите номер"
-        placeholder="Placeholder"
-        multiline
-        variant="standard"
-      />
-      <TextField
-        id="standard-textarea"
-        label="Введите email"
-        placeholder="Placeholder"
-        multiline
-        variant="standard"
-      />
-      <Button className="button">Оставить заявку</Button>
+      <div className="form">
+        <TextField
+          id="standard-textarea"
+          label="Введите номер"
+          placeholder="Placeholder"
+          multiline
+          variant="standard"
+        />
+        <TextField
+          id="standard-textarea"
+          label="Введите email"
+          placeholder="Placeholder"
+          multiline
+          variant="standard"
+        />
+        <Button className="button">Оставить заявку</Button>
+      </div>
     </div>
   );
 }
