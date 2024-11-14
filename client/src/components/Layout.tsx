de213d145c1b58736a206748b6f2eb462471f303
Дмитрий <euryalle@Noutbuk-Dmitrij.local>
2024-11-14 14:22:06 +0300

    basic layout

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import NavBar from './UI/NavBar';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout(): React.JSX.Element {
   return (
-    <div>Layout</div>
-  )
+    // <div>Layout</div>
+    <div>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
 }
