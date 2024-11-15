656238e2abb069abbc9bc3ba2ec7da05bb32672e
pugsparty <luka61097@yandex.ru>
2024-11-15 15:19:40 +0300

    release advanteges

--- client/src/components/pages/CompanyDescriptionPage.tsx
+++ client/src/components/pages/CompanyDescriptionPage.tsx
@@ -3,6 +3,8 @@ import svg from '../../img/15re_shop.svg';
 import svg2 from '../../img/education_location.svg';
 import svg3 from '../../img/Tilda_Icons_37_Finance_technologies_moneybag.svg';
 import svg4 from '../../img/GalochkaSvg.svg';
+import svg5 from '../../img/CompanyDescriptionImg.jpeg';
+import svg6 from '../../img/BabyArea.jpg';
 import '../../Styles/CompanyDescriptionPageStyle.css';
 export default function CompanyDescriptionPage(): React.JSX.Element {
   return (
@@ -37,12 +39,51 @@ export default function CompanyDescriptionPage(): React.JSX.Element {
         <div className="withoutPodryad">
           <h1>Земельные участки без подряда</h1>
           <h2>Стоимость земли без подряда 800 тыс руб/сотка (строитесь самостоятельно)</h2>
-          <span>
-            <img src={svg4}></img>строительство коттеджа возможно по своему проекту
-          </span>
           <br></br>
           <span>
             <img src={svg4}></img>при бронировании участка с подрядом - проект коттеджа в подарок
+          </span>
+          <span>
+            <img src={svg4}></img>строительство коттеджа возможно по своему проекту
+          </span>
+          <img className="imgWithoutPodryad" src={svg5}></img>
+        </div>
+        <div className="advantages">
+          <img className="imgAdvantages" src={svg6}></img>
+          <h2 className="advantagesTitle">
+            <img className="imgTitle" src={svg4}></img>Чистота сделки
+          </h2>
+          <span className="advantagesText">
+            Земля находится в собственности, сделка происходит с продавцом по Договору купли-продажи
+          </span>
+          <h2 className="advantagesTitle">
+            <img className="imgTitle" src={svg4}></img>Возможность регистрации
+          </h2>
+          <span className="advantagesText">
+            Территория относится к землям населенных пунктов для индивидуального жилищного
+            строительства (ИЖС)
+          </span>
+          <h2 className="advantagesTitle">
+            <img className="imgTitle" src={svg4}></img>Оформление в собственность
+          </h2>
+          <span className="advantagesText">
+            Вы становитесь владельцем участка, а затем построенного на нем дома.
+          </span>
+          <h2 className="advantagesTitle">
+            <img className="imgTitle" src={svg4}></img>Дорога
+          </h2>
+          <span className="advantagesText">Aсфальт/брусчатка с тротуарами</span>
+          <h2 className="advantagesTitle">
+            <img className="imgTitle" src={svg4}></img>Инфраструктура
+          </h2>
+          <span className="advantagesText">
+            Детская площадка, спортивные площадки, теннисный корт
+          </span>
+          <h2 className="advantagesTitle">
+            <img className="imgTitle" src={svg4}></img>Безопасность
+          </h2>
+          <span className="advantagesText">
+            Закрытая, охраняемая территория, въездная группа, КПП
           </span>
         </div>
       </div>
--- client/src/components/pages/ContactsPage.tsx
+++ client/src/components/pages/ContactsPage.tsx
@@ -5,7 +5,7 @@ import '../../Styles/ContactsPageStyle.css';
 
 export default function ContactsPage(): React.JSX.Element {
   return (
-    <div className="container">
+    <div className="containerContacts">
       <div className="text">
         <h1 className="contacts">КОНТАКТЫ:</h1>
         <h2 className="phone">+7 (915) 141-97-77</h2>
